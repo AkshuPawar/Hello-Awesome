e71595b7d05f7289044d129afd5bf179c0a324df
AkshuPawar <102858691+AkshuPawar@users.noreply.github.com>
2022-12-27 11:40:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end('Hello Awesome - Welcome to GCP Cloud Build Service')
+    res.end('Hello Awesome - Welcome to GCP ')
 })
 const PORT = process.env.PORT || 8080
 server.listen(PORT, () => console.log('Listening'))
